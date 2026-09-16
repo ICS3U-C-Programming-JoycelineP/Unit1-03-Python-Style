984c7cf0cd1dd4b24fae32a37697d6ecfdaff6d5
Joyceline Pangning <joyceline.pangning-mfadja@stu.ocsb.ca>
2026-09-16 02:31:22 +0000

    First commit of hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Pangning Joyceline
+# Date: September 15, 2026
+# Write This Program displays a greeting.and contains proper formatting
+def main():
+    print("Hello, Ms. Raffin")
+    print("Programming is So Interesting!")
+
+
+if __name__ == "__main__":
+    main()
